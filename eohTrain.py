93ac705dc66d78fa9c762dd96d94782b2c31d831
Owen Hunger <97658961+ohunger@users.noreply.github.com>
2024-04-06 12:09:31 -0700

    update wordcloud
    still seems a little bit off do I have to process more?

--- eohTrain.py
+++ eohTrain.py
@@ -25,14 +25,64 @@ label: a label that marks the article as potentially unreliable
 
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
-
-
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.svm import SVC
+import matplotlib.pyplot as plt
 from wordcloud import WordCloud
 
 
 def main():
 
     df = pd.read_csv('train.csv')
+    df.dropna(inplace=True)
+    df.reset_index(inplace=True)
+    #--------------------------------------------------
+    # Separate titles and article bodies based on label
+    reliable_df = df[df['label'] == 0]
+    unreliable_df = df[df['label'] == 1]
+
+    # Concatenate titles and article bodies for reliable and unreliable articles
+    reliable_titles = ' '.join(reliable_df['title'].astype(str))
+    unreliable_titles = ' '.join(unreliable_df['title'].astype(str))
+
+    reliable_bodies = ' '.join(reliable_df['text'].astype(str))
+    unreliable_bodies = ' '.join(unreliable_df['text'].astype(str))
+
+    # Create word clouds for reliable and unreliable titles
+    reliable_title_wordcloud = WordCloud(width=800, height=400, background_color='white').generate(reliable_titles)
+    unreliable_title_wordcloud = WordCloud(width=800, height=400, background_color='white').generate(unreliable_titles)
+
+    # Create word clouds for reliable and unreliable article bodies
+    reliable_body_wordcloud = WordCloud(width=800, height=400, background_color='white').generate(reliable_bodies)
+    unreliable_body_wordcloud = WordCloud(width=800, height=400, background_color='white').generate(unreliable_bodies)
+
+    # Plot word clouds
+    plt.figure(figsize=(14, 10))
+
+    plt.subplot(2, 2, 1)
+    plt.imshow(reliable_title_wordcloud, interpolation='bilinear')
+    plt.title('Word Cloud for Reliable Titles')
+    plt.axis('off')
+
+    plt.subplot(2, 2, 2)
+    plt.imshow(unreliable_title_wordcloud, interpolation='bilinear')
+    plt.title('Word Cloud for Unreliable Titles')
+    plt.axis('off')
+
+    plt.subplot(2, 2, 3)
+    plt.imshow(reliable_body_wordcloud, interpolation='bilinear')
+    plt.title('Word Cloud for Reliable Article Bodies')
+    plt.axis('off')
+
+    plt.subplot(2, 2, 4)
+    plt.imshow(unreliable_body_wordcloud, interpolation='bilinear')
+    plt.title('Word Cloud for Unreliable Article Bodies')
+    plt.axis('off')
+
+    plt.tight_layout()
+    plt.show()
+    #----------------------------------------------------
+    '''
     df.dropna(inplace=True)
     df.reset_index(inplace=True)
     print(df.describe())
@@ -54,7 +104,7 @@ def main():
     # SO we could start by splitting into two variables depending on whether they are real or fake, and show the most common words in the real titles, and then another plot
     # for the words in the fake title.
 
-    
+    '''
 
 
 
