9059b3545752e4af822cc8ea9de29bb4cc8ba410
Hetanshee <hetansheeshah@gmail.com>
2024-04-30 21:16:55 -0700

    Updated with necessary refactoring.

--- recognition.py
+++ recognition.py
@@ -1,9 +1,9 @@
-import tensorflow as tf 
+import tensorflow as tf
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.text import one_hot
 from tensorflow.keras.preprocessing.sequence import pad_sequences
-import numpy as np 
-import pandas as pd 
+import numpy as np
+import pandas as pd
 
 test_title = ["spark an inner revolution"]
 
